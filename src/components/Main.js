a424b24a9c79f3087e39aba75935ddd417ceedae
Shreyansh Srivastava <shreyanshsri1807@gmail.com>
2025-05-16 01:24:35 +0530

    Complete Why choose Us section

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './Navbar'
 import Hero from './Hero'
 import HowItWorks from './HowItWorks'
+import WhyCooseUs from './WhyCooseUs'
 
 function Main() {
   return (
@@ -9,6 +10,7 @@ function Main() {
         <Navbar/>
         <Hero/>
         <HowItWorks/>
+        <WhyCooseUs/>
     </div>
   )
 }
